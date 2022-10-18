b939863c26bfeaa51a3b05f618707b906b96e82b
KwonSeongMin1 <smin9011@gmail.com>
2022-10-18 18:35:39 +0900

    Initial Commit

---
+++ codingTestWithC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  codingTestWithC
+//
+//  Created by 권성민 on 2022/10/18.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
